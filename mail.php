8b0137b6f0512707c7b5a54a741724c65b20ee53
sadiqalimir <astrosadiqa@gmail.com>
2023-08-13 12:36:26 +0530

    copy upload 405

---
+++ mail.php
@@ -0,0 +1,52 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+//Script Foreach
+$c = true;
+if ( $method === 'POST' ) {
+
+	$project_name = trim($_POST["project_name"]);
+	$sadiqalimir.ae21@rvce.edu.in  = trim($_POST["sadiqalimir.ae21@rvce.edu.in"]);
+	$form_subject = trim($_POST["form_subject"]);
+
+	foreach ( $_POST as $key => $value ) {
+		if ( $value != "" && $key != "project_name" && $key != "sadiqalimir.ae21@rvce.edu.in" && $key != "form_subject" ) {
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+		}
+	}
+} else if ( $method === 'GET' ) {
+
+	$project_name = trim($_GET["project_name"]);
+	$sadiqalimir.ae21@rvce.edu.in  = trim($_GET["sadiqalimir.ae21@rvce.edu.in"]);
+	$form_subject = trim($_GET["form_subject"]);
+
+	foreach ( $_GET as $key => $value ) {
+		if ( $value != "" && $key != "project_name" && $key != "sadiqalimir.ae21@rvce.edu.in" && $key != "form_subject" ) {
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+		}
+	}
+}
+
+$message = "<table style='width: 100%;'>$message</table>";
+
+function adopt($text) {
+	return '=?UTF-8?B?'.Base64_encode($text).'?=';
+}
+
+$headers = "MIME-Version: 1.0" . PHP_EOL .
+"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+'From: '.adopt($project_name).' <'.$sadiqalimir.ae21@rvce.edu.in.'>' . PHP_EOL .
+'Reply-To: '.$sadiqalimir.ae21@rvce.edu.in.'' . PHP_EOL;
+
+mail($sadiqalimir.ae21@rvce.edu.in, adopt($form_subject), $message, $headers );
